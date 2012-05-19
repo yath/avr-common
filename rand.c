b94cf519cd0a71c1be470d5251e012140c905fda
Sebastian Schmidt <yath@yath.de>
2012-05-19 16:38:45 +0200

    Make rand() return an unsigned int
    Make rand() return an unsigned int so more optimizations (like
    doing bit-ands/-shifts instead of divisions/modulo) will work.
    
    Cast to signed, if desired.

--- rand.c
+++ rand.c
@@ -6,7 +6,7 @@ void srand(unsigned long s) {
      seed = s;
  } 
 
-int rand(void) {
+unsigned int rand(void) {
     seed = seed * 1664525L + 1013904223L;   
-    return ((int)((seed>> 17) & 0x7fff));
+    return ((unsigned int)((seed>> 17) & 0x7fff));
 }
--- rand.h
+++ rand.h
@@ -1,5 +1,5 @@
 #ifndef _COMMON_RAND_H_
 #define _COMMON_RAND_H_
 void srand(unsigned long);
-int rand(void);
+unsigned int rand(void);
 #endif /* _COMMON_RAND_H_ */
